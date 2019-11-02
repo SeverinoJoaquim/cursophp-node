554cedfe49b39cf72bf168c90df7456ba27f306b
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-02 18:38:50 -0300

    Aulas 15 e 16 - Funções do array 2

--- 09-funcoes-de-arrays.php
+++ 09-funcoes-de-arrays.php
@@ -37,3 +37,43 @@ echo "<br>";
 $parentes = array("Pai"=>"Joaquim", "Filha"=>"Lorrayne", "Mãe"=>"Maria José", "Avó"=>"Cecília", "Amiga"=>"Kamila");
 $keys = array_keys($parentes);
 print_r($keys);
+echo "<hr>";
+echo "<br>";
+
+// Funções de arrays - Aula 16
+// array_values($array) - retorna um novo array com os valores do array passado como parâmetro
+$parentes = array("Pai"=>"Joaquim", "Filha"=>"Lorrayne", "Mãe"=>"Maria José", "Avó"=>"Cecília", "Amiga"=>"Kamila");
+$values = array_values($parentes);
+print_r($values);
+echo "<hr>";
+echo "<br>";
+
+//array_marge($array1, $array2) - agraga o conteúdo de dois arrays
+$carros = array("Camaro", "Uno", "Gol", "Jeep");
+$motos = array("pop100", "cb1000", "kawasak", "dimp");
+
+$veiculos = array_merge($carros, $motos);
+print_r($veiculos);
+echo "<hr>";
+echo "<br>";
+
+// array_pop - exclui a última posição do array
+print_r($carros);
+echo "<br>";
+echo "<br>";
+echo array_pop($carros);
+echo "<br>";
+print_r($carros);
+echo "<hr>";
+echo "<br>";
+
+//array_shift - exclui a primeira posição do array
+
+print_r($carros);
+echo "<br>";
+echo "<br>";
+echo array_shift($carros);
+echo "<br>";
+print_r($carros);
+echo "<hr>";
+echo "<br>";
