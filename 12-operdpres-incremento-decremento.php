31ea8aa53b332a07718f149947343b5f5a8e0eae
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-03 00:12:10 -0300

    Aula 22 - Operadres de incremento e decremento

---
+++ 12-operdpres-incremento-decremento.php
@@ -0,0 +1,29 @@
+<?php
+// Operadores de incremento e decremento - Aula 22
+$valor = 20;
+
+//Pré-incremento
+echo ++$valor;
+echo "<hr>";
+echo "<br>";
+////////////////////////
+//Pós-incremento
+$valor2 = 50;
+echo $valor2++;
+echo "<br>";
+echo $valor2;
+echo "<hr>";
+echo "<br>";
+
+////////////////////////
+//Pré-decremento
+$valor3 = 30;
+echo --$valor3;
+echo "<hr>";
+echo "<br>";
+
+//Pós-decremento
+$valor4 = 40;
+echo $valor4--;
+echo "<br>";
+echo $valor4;
