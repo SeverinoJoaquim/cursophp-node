0209fe63867bfa4ce720e36a77dca3f7fd3fb0c4
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-03 00:56:29 -0300

    Aula 24 - Operadres de comparação

---
+++ 14-operadores-de-comparacao.php
@@ -0,0 +1,44 @@
+<?php
+// Operadores de comparação - Aula 24
+// == (igualdade)
+// != (não igual)
+// === (identidade)
+// !===
+// <>
+// <php
+// >
+// <=
+// >=
+// <=>
+
+if(10 == 10):
+	echo "Positivo";
+else:
+	echo "Negativo";
+endif;
+echo "<hr>";
+echo "<br>";
+
+if(10 != 10):
+	echo "Positivo";
+else:
+	echo "Negativo";
+endif;
+echo "<hr>";
+echo "<br>";
+
+if(10 === "10"):
+	echo "Positivo";
+else:
+	echo "Negativo";
+endif;
+echo "<hr>";
+echo "<br>";
+
+if(10 !== "10"):
+	echo "Positivo";
+else:
+	echo "Negativo";
+endif;
+echo "<hr>";
+echo "<br>";
