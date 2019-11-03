8c3a391c7766f64fadfee288353561cad8dc027b
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-02 23:50:10 -0300

    Aula 21 - Operadres aritméticos

---
+++ 11-operadores-aritmeticos.php
@@ -0,0 +1,67 @@
+<?php
+/* Operadores Aritméticos - Aula 21
+Adição(+)
+Subtração(-)
+Multiplicação(*)
+Divisão(/)
+Módulo(%)
+Exponenciação(**)
+*/
+echo 10 + 30;
+echo "<hr>";
+echo "<br>";
+
+echo 50 + 30;
+echo "<hr>";
+echo "<br>";
+
+echo 50 / 2;
+echo "<hr>";
+echo "<br>";
+
+echo 10 * 30;
+echo "<hr>";
+echo "<br>";
+
+echo 10 % 3;
+echo "<hr>";
+echo "<br>";
+
+echo 2 ** 3;
+echo "<hr>";
+echo "<br>";
+////////////////////////////
+// Usando variáves
+$a = 10;
+$b = 20;
+$c = 40;
+$d = 50;
+$e = 5;
+
+$adicao = $b + $d;
+echo $adicao;
+echo "<hr>";
+echo "<br>";
+
+$subtracao = $d - $b;
+echo $subtracao;
+echo "<hr>";
+echo "<br>";
+
+$divisao = $d / $a;
+echo $divisao;
+echo "<hr>";
+echo "<br>";
+
+$restodiv = $d % $a;
+echo $restodiv;
+echo "<hr>";
+echo "<br>";
+
+$produto = $d * $b;
+echo $produto;
+echo "<hr>";
+echo "<br>";
+
+$potencia = $a ** $e;
+echo $potencia;
