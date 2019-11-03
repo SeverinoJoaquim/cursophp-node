7bc92e4bc8fcc12506f0d56d9a95d17c2e5b6428
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-02 23:18:09 -0300

    Aula 20 - Condicionais

--- 10-condicionais.php
+++ 10-condicionais.php
@@ -19,3 +19,29 @@ echo "<br>";
 echo "<hr>";
 $media = 6;
 echo ($media >= 7) ? "Aprovado!" : "Lamento. Reprovado!";
+echo "<br>";
+echo "<hr>";
+///////////////////////////////////////
+/* condicionais - Aula 20
+** switch
+** case
+*/
+
+$cor = "azul";
+switch ($cor):
+	case "vermelha":
+	echo "sua cor preferida é vermelha";
+	break;
+
+	case "verde":
+	echo "sua cor preferida é verde";
+	break;
+
+	case "azul":
+	echo "sua cor preferida é azul";
+	break;
+
+	default:
+	echo "Sua cor preferida não é vermelha, verde ou azul.";
+
+endswitch;
