84e8b405829868451b10ce99ed304e7a73504aca
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-03 00:38:48 -0300

    Aula 23 - Operadres de atribuição

---
+++ 13-operadores-atribuicao.php
@@ -0,0 +1,39 @@
+<?php
+// Operadpres de atribuição - Aula 23
+$a = 10;
+$b = 5;
+
+$a = $a + $b;
+echo $a;
+echo "<hr>";
+echo "<br>";
+
+$c = 10;
+$d = 5;
+
+$c += $d;
+echo $c;
+echo "<hr>";
+echo "<br>";
+
+$e = 10;
+$f = 5;
+
+$e -= $f;
+echo $e;
+echo "<hr>";
+echo "<br>";
+
+$g = 10;
+$h = 5;
+
+$g *= $h;
+echo $g;
+echo "<hr>";
+echo "<br>";
+
+$i = 10;
+$j = 5;
+
+$i **= $j;
+echo $i;
