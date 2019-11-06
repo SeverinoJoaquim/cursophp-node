9cccf36411b00f51a347943eef68a36541c3bddf
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-05 23:58:17 -0300

    Aula 44 - Include e Require

---
+++ 27-include-e-require/header.php
@@ -0,0 +1,7 @@
+<html>
+	<head>
+		<title>Include e Require</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+
---
+++ 27-include-e-require/index.php
@@ -0,0 +1,8 @@
+<?php
+include_once 'header.php';
+?>
+<?php echo "Olá, pessoal"; ?>
+
+<?php
+include_once 'footer.php';
+?>
