55a98d434d0d390c9f88b18ed33bb2c32f0ec4d6
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-05 23:32:19 -0300

    Aula 43 - Senha segura password_hash)

--- 26-criptografia.php
+++ 26-criptografia.php
@@ -13,3 +13,17 @@ echo "Sua senha em Md5 é: ". md5($senha);
 echo "<hr>";
 echo "<br>";
 echo "Sua senha em Sha1 é: ". Sha1($senha);
+//////////////////////////////////////////////
+echo "<hr>";
+echo "Senha segura <br>";
+$senha2 = "123456";
+$senha_db = '$2y$10$kMjQCfbyf.he.rpIa3K73udbMXBuq6LzkqYlyTi9gVrIyVxWoiduK';
+
+$senhaSegura = password_hash($senha2, PASSWORD_DEFAULT);
+echo $senhaSegura;
+echo "<br><br>";
+if(password_verify($senha2, $senha_db)):
+	echo "Senha válida!";
+else:
+	echo "Lamento. Senha inválida!";
+endif;
