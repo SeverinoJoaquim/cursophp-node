e06af4835ca4b96a53d83a76e1b5c194ad91af42
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-06 00:58:15 -0300

    Aula 47 - Criando um CRUD com Materialize

--- 28-crud/index.php
+++ 28-crud/index.php
@@ -1,19 +1,39 @@
-<!DOCTYPE html>
-  <html lang="pt-br">
-    <head>
-      <!--Import Google Icon Font-->
-      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-      <!--Import materialize.css-->
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<?php
+/// Header
+include_once 'includes/header.php';
+?>
 
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    </head>
+<div class="row">
+  <div class="col s12 m6 push-m3">
+    <h3 class="light">Clientes</h3>
+    <table class="striped">
+      <thead>
+        <tr>
+          <th>Nome</th>
+          <th>Sobrenome</th>
+          <th>Emal</th>
+          <th>Idade</th>
+        </tr>
+      </thead>
 
-    <body>
+      <tbody>
+        <tr>
+          <td>Severino</td>
+          <td>Joaquim</td>
+          <td>severinojoaquim@outlook.com</td>
+          <td>54</td>
+          <td><a href="" class="btn-floating orange"><i class="material-icons">edit</a></td>
+          <td><a href="" class="btn-floating red"><i class="material-icons">delete</a></td>
+        </tr>
+      </tbody>
+    </table>
+    <br>
+    <a href="" class="btn">Adicionar Cliente</a>
+  </div>
+</div>
 
-      <!--JavaScript at end of body for optimized loading-->
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-    </body>
-  </html>
+<?php
+/// Footer
+include_once 'includes/footer.php';
+?>
         
