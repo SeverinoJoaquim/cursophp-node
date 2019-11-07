0516980db2bcd57c2456f5ec222cace4b10ebfb5
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-07 17:30:03 -0300

    Aula 48 - Criando um CRUD 2

--- 28-crud/index.php
+++ 28-crud/index.php
@@ -28,7 +28,7 @@ include_once 'includes/header.php';
       </tbody>
     </table>
     <br>
-    <a href="" class="btn">Adicionar Cliente</a>
+    <a href="adicionar.php" class="btn">Adicionar Cliente</a>
   </div>
 </div>
 
