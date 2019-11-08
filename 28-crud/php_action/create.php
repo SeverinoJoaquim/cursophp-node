017143f72b90f787c8fd22ea869ae17eb681f250
SeverinoJoaquim <severinojoca@gmail.com>
2019-11-08 00:58:06 -0300

    Aula 51 - XSS (Cross Site Scripting)

--- 28-crud/php_action/create.php
+++ 28-crud/php_action/create.php
@@ -3,12 +3,21 @@
 session_start();
 // Conexão
 require_once 'db_connect.php';
+// Clear
+function clear($input) {
+	global Econnect;
+	// sql
+	$var = mysqli_escape_string($connect, $input);
+	// xss
+	$var = htmlspecialchars($var);
+	return $var;
+}
 
 if(isset($_POST['btn-cadastrar'])):
-	$nome = mysqli_escape_string($connect, $_POST['nome']);
-	$sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
-	$email = mysqli_escape_string($connect, $_POST['email']);
-	$idade = mysqli_escape_string($connect, $_POST['idade']);
+	$nome = clear($connect, $_POST['nome']);
+	$sobrenome = clear($connect, $_POST['sobrenome']);
+	$email = clear($connect, $_POST['email']);
+	$idade = clear($connect, $_POST['idade']);
 	
 	$sql = "INSERT INTO clientes (nome, sobrenome, email, idade) VALUES ('$nome', '$sobrenome', '$email', '$idade')";
 
